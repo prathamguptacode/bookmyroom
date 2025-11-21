59f8a2c38cb224f7f85525bb4c087d7f958ca6bb
Pratham Gupta <prathamgupta.wk@gmail.com>
2025-11-21 22:58:12 +0530

    navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,29 +1,33 @@
 import info from '../assets/info.svg'
 import mystyle from './navbar.module.css'
+import clsx from 'clsx'
+
 function Navbar() {
     return (
         <div className={mystyle.navbar}>
             <div className={mystyle.wrapperNav}>
 
-            
-            <div className={mystyle.uppernav}>
-                <div className={mystyle.logobx}>
-                    bookmyRoom
+
+                <div className={mystyle.uppernav}>
+                    <div className={mystyle.logobx}>
+                        bookmyRoom
+                    </div>
+                    <div className={mystyle.rnuppernav}>
+                        <div className={clsx(mystyle.countrywrapper,mystyle.hover)}>
+                            <div className={mystyle.country}><img src="flag.png" alt="country-flag" className={mystyle.countryImg} /></div>
+                        </div>
+                        <div className={clsx(mystyle.info,mystyle.hover)}><img src={info} alt="info-svg" /></div>
+                        <button className={clsx(mystyle.addprop, mystyle.hover)}>List your property</button>
+                        <button className={mystyle.signup}>SIGN UP</button>
+                        <button className={mystyle.login}>LOG IN</button>
+                    </div>
                 </div>
-                <div className={mystyle.rnuppernav}>
-                    <div className={mystyle.country}><img src="flag.png" alt="country-flag" className={mystyle.countryImg} /></div>
-                    <div className={mystyle.info}><img src={info} alt="info-svg" /></div>
-                    <button className={mystyle.addprop}>List your property</button>
-                    <button className={mystyle.signup}>SIGN UP</button>
-                    <button className={mystyle.login}>LOG IN</button>
+                <div className={mystyle.lowernav}>
+                    <div className={clsx(mystyle.opt,mystyle.hover)}>Stays</div>
+                    <div className={clsx(mystyle.opt,mystyle.hover)}>Flights</div>
+                    <div className={clsx(mystyle.opt,mystyle.hover)}>Carpooling</div>
+                    <div className={clsx(mystyle.opt,mystyle.hover)}>Travel Buddies</div>
                 </div>
-            </div>
-            <div className={mystyle.lowernav}>
-                <div className={mystyle.opt}>Stays</div>
-                <div className={mystyle.opt}>Flights</div>
-                <div className={mystyle.opt}>Carpooling</div>
-                <div className={mystyle.opt}>Travel Buddies</div>
-            </div>
             </div>
         </div>
     )
